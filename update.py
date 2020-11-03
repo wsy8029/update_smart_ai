c33c97c296089bfd1ca43a6e8e11e01a9cce026d
wsy8029 <wsy8029@gmail.com>
2020-11-03 10:06:25 +0900

    Add complete message

--- update.py
+++ update.py
@@ -9,6 +9,7 @@ shutil.copytree('./ai-contents-gyro-car-master/src', '/home/pi/workspace/ai-cont
 # 자동 업데이터의 updater.sh 코드를 수정된 updater.sh 코드로 변경
 shutil.copy('./updater.sh', '/usr/src/pi-updater/updater.sh')
 
+print("업데이트가 완료되었습니다. Smart AI Kit을 재시작해주세요.")
 
 # pymodi 1.0.1 설치를 위한 requirements - pexpect
 
